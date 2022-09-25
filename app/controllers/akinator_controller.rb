8da2437659c59206ddb0009ac965d39eeb2e1281
Rin0107 <rinpei09107@gmail.com>
2022-09-25 22:35:49 +0900

    コントローラーに最低限の機能を記入（第三章分）

--- app/controllers/akinator_controller.rb
+++ app/controllers/akinator_controller.rb
@@ -2,6 +2,29 @@ class AkinatorController < ApplicationController
     protect_from_forgery except: [:food]
 
     def food
+        body = request.body.read
+
+        signature = request.env['HTTP_X_LINE_SIGNATURE']
+        unless client.validate_signature(body, signature)
+            return head :bad_request
+        end
+
+        events = client.parse_events_from(body)
+        # メッセージのtype,userId,message等の情報を連想配列でeventsに代入
+        events.each do |event|
+            case event
+                when Line::Bot::Event::Message
+                    case event.type
+                        when Line::Bot::Event::MessageType::Text
+                            message = {
+                                type: 'text',
+                                text: event.message['text']
+                            }
+                            client.reply_message(event['replyToken'], message)
+                    end
+            end
+        end
+        head :ok
     end
 
     private
