0eb82ccc6e877831091a20883c277168f155627a
Rin0107 <rinpei09107@gmail.com>
2022-10-09 18:16:46 +0900

    test用routes更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/akinator_index' => 'akinator#index'
+  get 'akinator/index'
   post '/akinator_food' => 'akinator#food'
 end
