abc97a4981e8966cdf9c2ee3eecc9e8c7dcb14df
Rin0107 <rinpei09107@gmail.com>
2022-10-10 12:46:39 +0900

    db:migrate candidates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_10_030743) do
+ActiveRecord::Schema.define(version: 2022_10_10_034213) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "progress_id"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2022_10_10_030743) do
     t.datetime "updated_at", null: false
     t.index ["progress_id"], name: "index_answers_on_progress_id"
     t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
+  create_table "candidates", force: :cascade do |t|
+    t.integer "progress_id"
+    t.integer "solution_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["progress_id"], name: "index_candidates_on_progress_id"
+    t.index ["solution_id"], name: "index_candidates_on_solution_id"
   end
 
   create_table "features", force: :cascade do |t|
