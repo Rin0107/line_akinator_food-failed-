b3ef97daf047fbcc482441f7ec18bb6af81ce8ff
Rin0107 <rinpei09107@gmail.com>
2022-10-10 12:06:54 +0900

    Answer db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_10_025752) do
+ActiveRecord::Schema.define(version: 2022_10_10_030304) do
+
+  create_table "answers", force: :cascade do |t|
+    t.integer "answer_id"
+    t.integer "question_id"
+    t.float "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["answer_id"], name: "index_answers_on_answer_id"
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "features", force: :cascade do |t|
     t.integer "question_id"
