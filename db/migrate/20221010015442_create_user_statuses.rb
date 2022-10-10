565fd36c0bff3939fd6e07540302a825998ffd21
Rin0107 <rinpei09107@gmail.com>
2022-10-10 11:10:31 +0900

    UserStatusのmigrationファイル完成

--- db/migrate/20221010015442_create_user_statuses.rb
+++ db/migrate/20221010015442_create_user_statuses.rb
@@ -2,7 +2,7 @@ class CreateUserStatuses < ActiveRecord::Migration[5.2]
   def change
     create_table :user_statuses do |t|
       t.string :user_id
-
+      t.integer :status, default: 0, limit: 1
       t.timestamps
     end
   end
