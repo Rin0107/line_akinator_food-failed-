f5c58ac2ce0a3ddf306469747ba1d61d64df551b
Rin0107 <rinpei09107@gmail.com>
2022-10-11 22:27:24 +0900

    seed編集完了

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,28 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 case ENV["SECTION"]
-    when "f-1"
+    when "f_1"
         add_solutions = [
-            "すし"
+            "懐石・会席料理"
         ]
-    when "q-1"
+    when "f_2"
+        add_solutions = [
+            
+        ]
+    when "q_1"
         add_questions = [
             "和食の気分ですか？",
             "中華もあり？"
         ]
         add_features_matrix = [
             [1.0, -1.0]
+        ]
+    when "q_2"
+        add_questions = [
+
+        ]
+        add_features_matrix = [
+
         ]
 end
 
@@ -35,8 +46,8 @@ if add_questions.present? && add_features_matrix.present?
         # 最新のSolutionのレコード数とadd_features_matrixの要素数が一致する場合
 
         a_f_m_length_array = []
-        add_features_matrix.each do |f|
-            a_f_m_length_array = a_f_m_length_array.push(add_features_matrix[f].length)
+        for f in 1..add_features_matrix.length
+            a_f_m_length_array = a_f_m_length_array.push(add_features_matrix[f-1].length)
             # add_features_matrixの二次元要素（？）数を配列に、add_features_matrixの一次元要素数だけ追加
         end
 
@@ -51,12 +62,13 @@ if add_questions.present? && add_features_matrix.present?
             added_q = Question.last(add_questions.length)
             for q_l in 1..added_q.length do
                 for s in solutions do
-                    value = add_features_matrix[s.id -1][q_l -1]
+                    value = add_features_matrix[s.id-1][q_l-1]
 
-                    f = Feature.create()
+                    f = Feature.new()
                     f.question = added_q[q_l -1]
                     f.solution = s
                     f.value = value
+                    f.save
                 end
             end
 
