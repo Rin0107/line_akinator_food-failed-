5b6fefac46366b05a5c97cca8e5cacd39c818b92
Rin0107 <rinpei09107@gmail.com>
2022-10-11 23:43:43 +0900

    migrate編集

--- db/migrate/20221010025302_create_solutions.rb
+++ db/migrate/20221010025302_create_solutions.rb
@@ -1,9 +1,8 @@
 class CreateSolutions < ActiveRecord::Migration[5.2]
   def change
     create_table :solutions do |t|
-      t.string :name
+      t.string :name, index: { unique: true }
       t.timestamps
     end
-    add_index :solutions, [:name], unipue: true
   end
 end
