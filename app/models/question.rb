0a11c33d5b41c82ea934b8ac1a3301411bc42c34
Rin0107 <rinpei09107@gmail.com>
2022-10-22 15:04:13 +0900

    Solutionモデル、Questionモデル→Featureにdependent::destroy

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
     has_many :latest_questions
     has_many :progresses, through: :latest_questions
-    has_many :features
+    has_many :features, dependent: :destroy
     has_many :answers
 end
--- app/models/solution.rb
+++ app/models/solution.rb
@@ -1,5 +1,5 @@
 class Solution < ApplicationRecord
-    has_many :features
+    has_many :features, dependent: :destroy
     has_many :candidates
     has_many :progresses, through: :candidates
 end
